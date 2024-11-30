e88687257f209f7a12c7caffaf32b21ebe8d357e
linciss <linardssils1@gmail.com>
2024-11-30 18:17:02 +0200

    header cmponetn

---
+++ exammm/admin/components/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.tailwindcss.com" defer></script>
+    <title>Admin</title>
+</head>
+<body>
+<header>
+        <a href="./" class="logo" id="logo-text">
+            <i class="fa fa-server"></i> Bibliotech
+        </a>
+        <div class="apply">
+            <a href="./" class="btn">Sākums</a>
+            <a href="./pieteikumi.php" class="btn">Grāmatas</a>
+            <a href="./" class="btn">PRO īpašnieki</a>
+            <a href="./lietotaji.php" class="btn">Lietotāji</a>
+            <a href="logout.php" class="btn"><i class="fa-solid fa-power-off"></i></a>
+        </div>
+    </header>
--- exammm/admin/index.php
+++ exammm/admin/index.php
@@ -4,17 +4,10 @@ session_start();
         header("Location: login.php");
         exit;
     }
+
+    require 'components/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com" defer></script>
-    <title>Admin</title>
-</head>
-<body>
-    <h1 class="text-3xl font-bold underline">Hello world</h1>
+
 </body>
 </html>
