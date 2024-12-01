6f8948201323aad6f54cc0803f2853005b3ee124
lincis <linardssils1@gmail.com>
2024-12-01 17:15:40 +0200

    styking changes foir footer

--- exammm/admin/components/footer.php
+++ exammm/admin/components/footer.php
@@ -1,6 +1,6 @@
 <footer>
-    <div class="mx-auto border-t-2 border-gray-500 w-screen fixed bottom-0 right-0">
-        <div class="container mx-auto p-4">
+    <div class="mx-auto border-t-2 border-gray-500 w-full bottom-0 mt-5">
+        <div class="container mx-auto p-2">
             <div class="flex flex-col justify-center items-center">
                 <div class="col-md-6">
                     <p>&copy; 2024 - Linards</p>
