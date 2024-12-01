e148af60c7488f16c711e355a17e8e54216ec617
lincis <linardssils1@gmail.com>
2024-12-01 14:39:53 +0200

    styling chanegs to header

--- exammm/admin/components/header.php
+++ exammm/admin/components/header.php
@@ -40,30 +40,30 @@
     <title><?php echo $pageTitle ?></title>
 </head>
 <body class="dark:bg-darkBody bg-body text-black dark:text-white">
-<header class="flex justify-between px-8 py-4 bg-nav dark:bg-darkNav">
+<header class="flex justify-between px-8 py-4 bg-nav dark:bg-darkNav border-b-2 border-gray-500 shadow-lg">
         <a href="./" class="flex flex-row gap-2 items-center text-2xl font-bold " id="logo-text">
             <i class="fa fa-server"></i> Bibliotech
         </a>
         <div class=" flex-row gap-2 items-center sm:flex hidden">
-            <a href="./" class="<?php echo ($pageTitle == 'Sākums') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Sākums</a>
-            <a href="./books.php" class="<?php echo ($pageTitle == 'Grāmatas') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Grāmatas</a>
-            <a href="./readers.php" class="<?php echo ($pageTitle == 'Lasītāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lasītāji</a>
-            <a href="./users.php" class="<?php echo ($pageTitle == 'Lietotāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lietotāji</a>
-            <a href="logout.php" class="px-2 py-1 rounded-md border border-gray-500 flex flex-row gap-2 items-center"><?php echo $_SESSION['lincisExam']?><i class="fa-solid fa-power-off"></i></a>
-            <a class="cursor-pointer px-2 py-1 border border-gray-500 rounded-md" id="darkModeToggle"><i class="fa-solid fa-moon" id="themeIcon"></i></a>
+            <a href="./" class="<?php echo ($pageTitle == 'Sākums') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500 hover:bg-orange-200 dark:hover:bg-zinc-700">Sākums</a>
+            <a href="./books.php" class="<?php echo ($pageTitle == 'Grāmatas') ? 'bg-orange-200 dark:bg-zinc-700 ' : '' ?> px-2 py-1 rounded-md border border-gray-500 hover:bg-orange-200 dark:hover:bg-zinc-700">Grāmatas</a>
+            <a href="./readers.php" class="<?php echo ($pageTitle == 'Lasītāji') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500 hover:bg-orange-200 dark:hover:bg-zinc-700">Lasītāji</a>
+            <a href="./users.php" class="<?php echo ($pageTitle == 'Lietotāji') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500 hover:bg-orange-200 dark:hover:bg-zinc-700">Lietotāji</a>
+            <a href="logout.php" class="px-2 py-1 rounded-md border border-gray-500 flex flex-row gap-2 items-center hover:bg-orange-200"><?php echo $_SESSION['lincisExam']?><i class="fa-solid fa-power-off"></i></a>
+            <a class="cursor-pointer px-2 py-1 border border-gray-500 rounded-md hover:bg-orange-200" id="darkModeToggle"><i class="fa-solid fa-moon" id="themeIcon"></i></a>
         </div>
         <div class="sm:hidden flex flex-row gap-4 items-center"> 
         <div class=" hamburger cursor-pointer ">
             <i class="fa-solid fa-bars"></i>
         </div>
-        <a class="cursor-pointer px-2 py-1 border border-gray-500 rounded-md" id="darkModeToggleMobile"><i class="fa-solid fa-moon" id="themeIconMobile"></i></a>
+        <a class="cursor-pointer px-2 py-1 border border-gray-500 rounded-md " id="darkModeToggleMobile"><i class="fa-solid fa-moon" id="themeIconMobile"></i></a>
     </div>
        
     </header>
     <div class="flex-col gap-4 items-center fixed bg-nav h-screen w-full hidden transition-all ease-in-out duration-300" id="menu">
-    <a href="./" class="<?php echo ($pageTitle == 'Sākums') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Sākums</a>
-            <a href="./books.php" class="<?php echo ($pageTitle == 'Grāmatas') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Grāmatas</a>
-            <a href="./readers.php" class="<?php echo ($pageTitle == 'Lasītāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lasītāji</a>
-            <a href="./users.php" class="<?php echo ($pageTitle == 'Lietotāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lietotāji</a>
+    <a href="./" class="<?php echo ($pageTitle == 'Sākums') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500">Sākums</a>
+            <a href="./books.php" class="<?php echo ($pageTitle == 'Grāmatas') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500">Grāmatas</a>
+            <a href="./readers.php" class="<?php echo ($pageTitle == 'Lasītāji') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lasītāji</a>
+            <a href="./users.php" class="<?php echo ($pageTitle == 'Lietotāji') ? 'bg-orange-200 dark:bg-zinc-700' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lietotāji</a>
             <a href="logout.php" class="px-2 py-1 rounded-md border border-gray-500 flex flex-row gap-2 items-center"><?php echo $_SESSION['lincisExam']?><i class="fa-solid fa-power-off"></i></a>
     </div>
