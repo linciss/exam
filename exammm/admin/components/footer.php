d6f0b1541ae2124b14d93bda0707ca5a04f0adba
lincis <linardssils1@gmail.com>
2024-12-01 14:34:33 +0200

    footer component

---
+++ exammm/admin/components/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class=" mx-auto border-t-2 border-gray-500">
+        <div class="container mx-auto mt-4">
+            <div class="flex flex-col justify-center items-center">
+                <div class="col-md-6">
+                    <p>&copy; 2024 - Linards</p>
+                </div>
+                <div class="col-md-6 text-right">
+                    <p>Izstrādāja <a href="https://github.com/linciss" target="_blank">Linards Sils</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
