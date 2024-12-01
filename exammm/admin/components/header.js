93b8687936bd66f4f06b894973b11fade085af6e
lincis <linardssils1@gmail.com>
2024-12-01 13:44:34 +0200

    dark mode implementation

--- exammm/admin/components/header.js
+++ exammm/admin/components/header.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const hamburger = document.querySelector('.hamburger');
   const menu = document.getElementById('menu');
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  document.documentElement.classList.toggle(
+    'dark',
+    localStorage.getItem('darkMode') === 'true',
+  );
+
+  darkModeToggle.addEventListener('click', function () {
+    document.documentElement.classList.toggle('dark');
+    localStorage.setItem(
+      'darkMode',
+      document.documentElement.classList.contains('dark'),
+    );
+  });
 
   hamburger.addEventListener('click', function () {
     menu.classList.toggle('show-menu');
--- exammm/admin/components/header.php
+++ exammm/admin/components/header.php
@@ -8,6 +8,7 @@
     <script>
         // tailiwnd for custom stuff 
     tailwind.config = {
+        darkMode: 'class',
         content: [
         './**/*.php',
         './**/*.html',
@@ -21,6 +22,7 @@
             extend: {
             colors: {
                 nav: '#F0EAE5',
+                body: '#F7F7F7',
             },
             },
     },
@@ -35,9 +37,9 @@
     </style>
     <title><?php echo $pageTitle ?></title>
 </head>
-<body>
+<body class="dark:bg-gray-900 bg-body">
 <header class="flex justify-between px-8 py-4 bg-nav">
-        <a href="./" class="flex flex-row gap-2 items-center" id="logo-text">
+        <a href="./" class="flex flex-row gap-2 items-center text-2xl font-bold" id="logo-text">
             <i class="fa fa-server"></i> Bibliotech
         </a>
         <div class=" flex-row gap-4 items-center sm:flex hidden">
@@ -46,13 +48,17 @@
             <a href="./readers.php" class="<?php echo ($pageTitle == 'Lasītāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lasītāji</a>
             <a href="./users.php" class="<?php echo ($pageTitle == 'Lietotāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lietotāji</a>
             <a href="logout.php" class="px-2 py-1 rounded-md border border-gray-500 flex flex-row gap-2 items-center"><?php echo $_SESSION['lincisExam']?><i class="fa-solid fa-power-off"></i></a>
+            <a class="cursor-pointer" id="darkModeToggle">dark</a>
         </div>
-        <div class="sm:hidden hamburger cursor-pointer">
+        <div class="sm:hidden flex flex-row gap-4 items-center"> 
+        <div class=" hamburger cursor-pointer ">
             <i class="fa-solid fa-bars"></i>
         </div>
-            
+        <a class="cursor-pointer" id="darkModeToggle">dark</a>
+    </div>
+       
     </header>
-    <div class="flex-col gap-4 items-center fixed bg-nav h-screen w-full flex sm:hidden" id="menu">
+    <div class="flex-col gap-4 items-center fixed bg-nav h-screen w-full hidden transition-all ease-in-out duration-300" id="menu">
     <a href="./" class="<?php echo ($pageTitle == 'Sākums') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Sākums</a>
             <a href="./books.php" class="<?php echo ($pageTitle == 'Grāmatas') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Grāmatas</a>
             <a href="./readers.php" class="<?php echo ($pageTitle == 'Lasītāji') ? 'bg-black ' : '' ?> px-2 py-1 rounded-md border border-gray-500">Lasītāji</a>
