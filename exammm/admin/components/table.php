6184bc4cc6d4108824b7fe8a414126f7d222a731
lincis <linardssils1@gmail.com>
2024-12-01 17:15:21 +0200

    table as component dynamic bcs yes

---
+++ exammm/admin/components/table.php
@@ -0,0 +1,6 @@
+<table class="min-w-full divide-y divide-gray-500 border-gray-500 ">
+    <thead class="bg-orange-200 dark:bg-zinc-700 border border-gray-500 rounded-t-md">
+        <tr id="<?php echo $tableHead ?>"></tr>
+    </thead>
+    <tbody class="border-x border-gray-500 bg-white  divide-gray-500 dark:bg-gray-800 dark:divide-gray-700 " id="<?php echo $tableBody ?>"></tbody>
+</table>
