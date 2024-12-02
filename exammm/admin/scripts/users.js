4962fa561de5d029b553c051bb7b12b00a3e6090
lincis <linardssils1@gmail.com>
2024-12-02 17:09:01 +0200

    users page

---
+++ exammm/admin/scripts/users.js
@@ -0,0 +1,185 @@
+$(document).ready(() => {
+  let edit = false;
+
+  const headings = [
+    'ID',
+    'Lietotājvārds',
+    'Vārds',
+    'Uzvārds',
+    'E-pasts',
+    'Tiesības',
+    '',
+  ];
+
+  $('#usersHead').append(
+    headings
+      .map((heading) => {
+        if (heading === 'E-pasts') {
+          return `<th class="sm:px-6 px-2 py-3 text-left text-xs font-medium uppercase tracking-wider md:table-cell hidden">${heading}</th>`;
+        } else {
+          return `<th class="sm:px-6 px-2 py-3 text-left text-xs font-medium uppercase tracking-wider">${heading}</th>`;
+        }
+      })
+      .join(''),
+  );
+
+  const fetchUsers = () => {
+    $.ajax({
+      url: 'database/users/users.php',
+      type: 'GET',
+      success: (res) => {
+        const users = JSON.parse(res);
+        let template = '';
+
+        users.forEach((user) => {
+          template += `<tr userId="${user.id}" class="bg-nav dark:bg-darkNav border-b border-gray-500 ">
+            <td class="sm:px-6 px-2 py-4 ">${user.id}</td>
+            <td class="sm:px-6 px-2 py-4 ">${user.username}</td>
+            <td class="sm:px-6 px-2 py-4 ">${user.name}</td>
+            <td class="sm:px-6 px-2 py-4 ">${user.lastName}</td>
+            <td class="sm:px-6 px-2 py-4 md:table-cell hidden">${user.email}</td>
+            <td class="sm:px-6 px-2 py-4 ">${user.role}</td>
+            <td class="sm:px-6 px-2 py-4 ">
+                <a id="userEdit" class="px-2 cursor-pointer py-1 border border-gray-500 bg-orange-200 dark:bg-zinc-700 rounded-md"><i class="fa-solid fa-pencil"></i></a>
+                <a id="userDelete" class="px-2 cursor-pointer py-1 border border-gray-500 bg-orange-200 dark:bg-zinc-700 rounded-md"><i class="fa-solid fa-trash"></i></a>
+            </td>
+        </tr>`;
+        });
+        $('#usersBody').html(template);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  };
+
+  fetchUsers();
+
+  const updateText = () => {
+    if (window.innerWidth < 515) {
+      $('#newuserButton').text('+');
+    } else {
+      $('#newuserButton').text('+ Pievienot lietotāju');
+    }
+  };
+
+  updateText();
+
+  $(window).resize(updateText);
+
+  const getTemplate = (edit) => {
+    return `
+    <div class="grid gap-4 grid-cols-2">
+              <label for="username" class="text-xl">Vārds:</label>
+              <input type="text" name="username" id="username" required class="shadow-md border border-gray-500 rounded-md p-1 dark:bg-zinc-900 bg-body">
+              <label for="name" class="text-xl">Vārds:</label>
+              <input type="text" name="name" id="name" required class="shadow-md border border-gray-500 rounded-md p-1 dark:bg-zinc-900 bg-body">
+              <label for="lastName" class="text-xl">Uzvārds:</label>
+              <input type="text" name="lastName" id="lastName" required class="shadow-md border border-gray-500 rounded-md p-1 dark:bg-zinc-900 bg-body">
+              <label for="email" class="text-xl">E-pasts:</label>
+              <input type="email" name="email" id="email" required class="shadow-md border border-gray-500 rounded-md p-1 dark:bg-zinc-900 bg-body">
+              <label  label for="role" class="text-xl">Tiesības:</label>
+              <select  name="role" id="role" class="shadow-md border border-gray-500 rounded-md p-1 dark:bg-zinc-900 bg-body">
+                 <option  value="admin">Administrators</option>
+                 <option value="moderator">Moderators</option>
+              </select>
+              <label for="password" class="text-xl">Parole:</label>
+              <input type="password" placeholder="*****" name="password" id="password" required class="shadow-md border border-gray-500 rounded-md p-1 dark:bg-zinc-900 bg-body">
+          </div>
+          <input type="hidden" name="id" id="id">
+          <button type="submit" class="w-full mt-4 border border-gray-500 p-2 rounded-md hover:bg-nav bg-orange-200  dark:bg-zinc-700 dark:hover:bg-darkNav">Pievienot</button>
+    `;
+  };
+
+  const refreshForm = () => {
+    $('#userForm').html(getTemplate(edit));
+  };
+
+  $('#newUserButton').click(() => {
+    $('#userModal').toggleClass('show-menu');
+    $('#userForm').trigger('reset');
+    $('#modalTitle').text('Pievienot lietotāju');
+    edit = false;
+    refreshForm();
+  });
+
+  $('#modalClose').click(() => {
+    $('#userModal').toggleClass('show-menu');
+    $('#userForm').trigger('reset');
+    $('#errorAdd').addClass('hidden');
+    $('#errorAdd').removeClass('!block');
+    edit = false;
+  });
+
+  $('#userForm').submit((e) => {
+    e.preventDefault();
+
+    const formData = {
+      username: $('#username').val(),
+      name: $('#name').val(),
+      lastName: $('#lastName').val(),
+      email: $('#email').val(),
+      role: $('#role').val(),
+      password: $('#password').val(),
+      id: $('#id').val() || null,
+    };
+
+    const url = edit
+      ? 'database/users/edit-user.php'
+      : 'database/users/add-user.php';
+
+    $.ajax({
+      url,
+      type: 'POST',
+      data: formData,
+      success: (res) => {
+        fetchUsers();
+        $('#userModal').toggleClass('show-menu');
+        $('#errorAdd').addClass('hidden');
+        $('#errorAdd').removeClass('!block');
+      },
+      error: (err) => {
+        $('#errorAdd').removeClass('hidden');
+        $('#errorAdd').toggleClass('!block');
+        $('#errorAdd').text(err.responseText);
+      },
+    });
+  });
+
+  $(document).on('click', '#userDelete', (e) => {
+    if (confirm('Vai tiešām vēlaties dzēst lietotāju?')) {
+      const element = $(e.currentTarget).closest('tr');
+      const id = element.attr('userId');
+
+      $.post('database/users/delete-user.php', { id }, (res) => {
+        fetchUsers();
+      });
+    }
+  });
+
+  // $(document).on('click', '#userEdit', (e) => {
+  //   edit = true;
+
+  //   $('#userModal').toggleClass('show-menu');
+  //   $('#userForm').trigger('reset');
+  //   $('#modalTitle').text('Rediģēt grāmatu');
+  //   refreshForm();
+
+  //   const element = $(e.currentTarget).closest('tr');
+  //   const id = element.attr('userId');
+
+  //   $.post('database/users/user.php', { id }, (res) => {
+  //     const user = JSON.parse(res);
+  //     console.log(user);
+  //     $('#title').val(user.title);
+  //     $('#author').val(user.author);
+  //     $('#genre').val(user.genre);
+  //     $('#release').val(user.releaseDate);
+  //     $('#description').val(user.description);
+  //     $('#inStorage').val(user.inStorage);
+  //     $('#id').val(user.id);
+  //     edit = true;
+  //   });
+  // });
+  console.log('helklo world');
+});
--- exammm/admin/users.php
+++ exammm/admin/users.php
@@ -4,14 +4,38 @@ session_start();
         header("Location: login.php");
         exit;
     }
-
     $pageTitle = 'Lietotāji';
+    $scriptName = 'users.js';
 
+    
+    if($_SESSION['role'] !== "admin"){
+        header("Location: ./");
+        exit;
+    }
     require 'components/header.php';
 ?>
+<section class="flex flex-col gap-8"> 
+ 
+ <?php 
+     $type = "Lietotāju";
+     $buttonType = "newUserButton";
+     require 'components/utils.php';
+ ?>
+
+ <div class="overflow-hidden container mx-auto rounded-md bg-white dark:bg-black px-4 py-8 text-black dark:text-white">
+     <?php
+     $tableHead = 'usersHead';
+     $tableBody = 'usersBody';
+     require 'components/table.php'; 
+     ?>
+ </div>
+</section>
 
 <?php
-    require 'components/footer.php';
+ $modalType = 'userModal';
+ $formType = 'userForm';
+ require 'components/modal.php';
+ require 'components/footer.php';
 ?>
 </body>
 </html>
