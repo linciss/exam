4235e31d798c17c0e3eb2c694be89fb35e36bf5c
lincis <linardssils1@gmail.com>
2024-12-02 20:39:34 +0200

    reader

--- exammm/admin/readers.php
+++ exammm/admin/readers.php
@@ -4,14 +4,34 @@ session_start();
         header("Location: login.php");
         exit;
     }
-
     $pageTitle = 'Lasītāji';
+    $scriptName = 'readers.js';
+
 
     require 'components/header.php';
 ?>
+<section class="flex flex-col gap-8"> 
+ 
+ <?php 
+     $type = "Lasītāju";
+     $buttonType = "newReaderButton";
+     require 'components/utils.php';
+ ?>
+
+ <div class="overflow-hidden container mx-auto rounded-md bg-white dark:bg-black px-4 py-8 text-black dark:text-white">
+     <?php
+     $tableHead = 'readersHead';
+     $tableBody = 'readersBody';
+     require 'components/table.php'; 
+     ?>
+ </div>
+</section>
 
 <?php
-    require 'components/footer.php';
+ $modalType = 'readerModal';
+ $formType = 'readerForm';
+ require 'components/modal.php';
+ require 'components/footer.php';
 ?>
 </body>
 </html>
