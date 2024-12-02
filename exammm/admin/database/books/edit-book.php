61c5646d709fb5e54d63f61e2d726332ae318fe4
lincis <linardssils1@gmail.com>
2024-12-02 20:39:59 +0200

    check if the book is takken so cant change the in stoirage prop

--- exammm/admin/database/books/edit-book.php
+++ exammm/admin/database/books/edit-book.php
@@ -12,13 +12,46 @@ if(isset($_POST['id'])){
     $cover = $_POST['cover'];
     $inStorage = htmlspecialchars($_POST['inStorage']);
 
+    $checkQuery = $con->prepare("SELECT * FROM ex_books WHERE book_id = ?");
+    $checkQuery->bind_param("i", $id);
+    $checkQuery->execute();
+    $result = $checkQuery->get_result();
+    $checkQuery->close();
+    $result = $result->fetch_assoc();
+    if(!$result){
+        die('Invalid query: ' . mysqli_error());
+    }
+    if($result['in_storage'] !== $inStorage){
+        $query = $con->prepare("SELECT * FROM ex_readers WHERE book_id = ?");
+        $query->bind_param("i", $id);
+        $query->execute();
+        $result = $query->get_result();
+        $query->close();
+        $result = $result->fetch_assoc();
+        if($result){
+            echo $_SESSION['error'] = 'Grāmata nav atgriezta!';
+            http_response_code(400);
+            exit;
+        }else{
+            $query = $con->prepare("UPDATE ex_readers SET status = 'atgriezts' WHERE book_id = ?");
+            $query->bind_param("i", $id);
+            $query->execute();
+            $query->close();
+        }
+       
+    }
+
+
+
+    
+
     $query = $con->prepare("UPDATE ex_books SET title = ?, author = ?, genre = ?, release_date = ?, cover_image = ?, description = ?, in_storage = ? WHERE book_id = ?");
 
     $query->bind_param("ssssbssi", $title, $author, $genre, $date, $cover, $description, $inStorage, $id);
 
 
     if($query->execute()){
-        echo 'Pieteikums redigets!';
+        echo 'GRamat  redigets!';
     }else{
         echo 'Kļūda!'. $query->error;
     }
