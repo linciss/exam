988d17e98d1b28315f4d6e7a721e39925e2b6f76
lincis <linardssils1@gmail.com>
2024-12-03 11:21:13 +0200

    hoem poage

--- exammm/admin/index.php
+++ exammm/admin/index.php
@@ -11,6 +11,59 @@ session_start();
     require 'components/header.php';
 ?>
 
+<div class="mt-5 overflow-hidden container mx-auto rounded-md bg-white dark:bg-black px-4 py-8 text-black dark:text-white">
+    <div class="grid lg:grid-flow-row lg:grid-cols-5 sm:grid-cols-3 grid-cols-1 gap-5">
+        <div class="bg-nav dark:bg-darkNav p-2 flex flex-col gap-2 justify-center items-center col-span-full lg:col-span-2 rounded-md">
+            <h2 class="text-3xl font-bold">Sveiki, <?php echo $_SESSION['lincisExam'] ?></h2>
+            <p class="text-lg">Jūs esat ielogojies kā <?php echo $_SESSION['role'] === "admin" ? "Administrators" : "Moderators" ?></p>
+        </div>
+
+        <div class="bg-nav dark:bg-darkNav p-2 flex flex-row justify-center items-center gap-4 rounded-md">
+            <div class="flex items-center justify-center w-10 h-10 bg-orange-200 dark:bg-zinc-700 rounded-full">
+                <i class="fa-solid fa-shield-halved text-xl"></i>
+            </div>
+            <div>
+                <p class="text-md font-semibold" id="waiting">3</p> 
+                <h3 class="text-lg font-semibold" >Lasītāji gaida</h3>
+            </div>
+        </div>
+        <div class="bg-nav dark:bg-darkNav p-2 flex flex-row justify-center items-center gap-4 rounded-md">
+            <div class="flex items-center justify-center w-10 h-10 bg-orange-200 dark:bg-zinc-700 rounded-full">
+                    <i class="fa-solid fa-shield-halved text-xl"></i>
+                </div>
+                <div>
+                    <p class="text-md font-semibold" id="reading">3</p> 
+                    <h3 class="text-lg font-semibold">Lasītāji apstiprināti</h3>
+                </div>
+            </div>
+        <div class="bg-nav dark:bg-darkNav p-2 flex flex-row justify-center items-center gap-4 rounded-md">
+            <div class="flex items-center justify-center w-10 h-10 bg-orange-200 dark:bg-zinc-700 rounded-full">
+                <i class="fa-solid fa-shield-halved text-xl"></i>
+            </div>
+            <div>
+                <p class="text-md font-semibold" id="inStorage">3</p> 
+                <h3 class="text-lg font-semibold">Grāmatas noliktavā</h3>
+            </div>
+        </div>
+    </div>
+    <div class="grid  grid-cols-1 xl:grid-cols-2 mt-5 gap-5">
+        <div class="text-center">
+            <h2 class="text-2xl font-bold mt-5 mb-2 bg-nav dark:bg-darkNav rounded-md">Jaunākie lasītāji</h2>
+            <?php
+                $tableHead = 'readersHead';
+                $tableBody = 'readersBody';
+                require 'components/table.php'; 
+             ?>
+        </div>
+        <div class="text-center ">
+            <h2 class="text-2xl font-bold mt-5 mb-2 bg-nav dark:bg-darkNav rounded-md">Pēdējie notikumi</h2>
+            <div>
+                <canvas id="chart"></canvas>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
     require 'components/footer.php';
 ?>
--- exammm/admin/scripts/index.js
+++ exammm/admin/scripts/index.js
@@ -1,3 +1,66 @@
 $(document).ready(() => {
-  console.log('Hello from index.js');
+  const headings = [
+    'ID',
+    'Vārds',
+    'Uzvārds',
+    'Reg. datums',
+    'Grāmata',
+    'Statuss',
+  ];
+
+  $('#readersHead').append(
+    headings
+      .map((heading) => {
+        return `<th class="sm:px-6 px-2 py-3 text-center text-xs font-medium uppercase tracking-wider ">${heading}</th>`;
+      })
+      .join(''),
+  );
+
+  const fetchReaders = () => {
+    $.ajax({
+      url: 'database/readers/readers.php',
+      type: 'GET',
+      success: (res) => {
+        const readers = JSON.parse(res);
+        let template = '';
+
+        $('#waiting').text(
+          readers.filter((reader) => reader.status === 'Gaida').length,
+        );
+
+        $('#reading').text(
+          readers.filter((reader) => reader.status === 'Apstiprināts').length,
+        );
+
+        $.get('database/books/books.php', (res) => {
+          const books = JSON.parse(res);
+
+          const booksInStorage = books.filter(
+            (book) => book.inStorage === 'Jā',
+          );
+
+          $('#inStorage').text(booksInStorage.length);
+        });
+
+        readers.forEach((reader) => {
+          template += `<tr readerId="${reader.id}" class="${
+            reader.deadlineExceeded ? 'bg-red-300' : 'bg-nav dark:bg-darkNav'
+          } border-b border-gray-500 ">
+              <td class="sm:px-6 px-2 py-4 ">${reader.id}</td>
+              <td class="sm:px-6 px-2 py-4 ">${reader.name}</td>
+              <td class="sm:px-6 px-2 py-4 ">${reader.lastName}</td>
+              <td class="sm:px-6 px-2 py-4 ">${reader.date}</td>
+              <td class="sm:px-6 px-2 py-4 ">${reader.book}</td>
+              <td class="sm:px-6 px-2 py-4 ">${reader.status}</td>
+          </tr>`;
+        });
+        $('#readersBody').html(template);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  };
+
+  fetchReaders();
 });
