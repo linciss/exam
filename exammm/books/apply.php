65ba63ed6615abe1719643adb8e93b94e95ec1e5
lincis <linardssils1@gmail.com>
2024-12-05 21:35:04 +0200

    refactor: Add form validation for book application page

--- exammm/books/apply.php
+++ exammm/books/apply.php
@@ -9,17 +9,25 @@ if(isset($_POST['bookId'])){
     $email = htmlspecialchars($_POST['email']);
     $bookId = intval($_POST['bookId']);
 
+    if(empty($name) || empty($lastName) || empty($phone) || empty($email) || empty($bookId)){
+        echo $_SESSION['error'] = 'Lūdzu aizpildiet visus laukus!';
+        http_response_code(400);
+        exit;
+    }
+
     $query = $con->prepare("SELECT reader_id FROM ex_readers WHERE email = ?");
     $query->bind_param("s", $email);
     $query->execute();
     $query->store_result();
 
+    
+
     if ($query->num_rows > 0) {
         $updateQuery = $con->prepare("UPDATE ex_readers SET name = ?, last_name = ?, phone = ?, book_id = ?, status='waiting', date_taken = NULL, date_returned = NULL, reg_date = NOW()  WHERE email = ?");
         $updateQuery->bind_param("sssis", $name, $lastName, $phone, $bookId, $email);
         if ($updateQuery->execute()) {
-            $_SESSION['success'] = 'Pieteikums pievienots!';
-            $_SESSION['successMessage'] = '
+            echo $_SESSION['success'] = 'Pieteikums pievienots!';
+           echo $_SESSION['successMessage'] = '
                 <p class="text-md">Dodieties uz mūsu bibliotēku Ventspils ielā 51. Jums ir 7 dienu laiks, lai grāmatu savāktu!</p>
             ';
         } else {
@@ -30,12 +38,12 @@ if(isset($_POST['bookId'])){
         $insertQuery = $con->prepare("INSERT INTO ex_readers (name, last_name, phone, email, book_id) VALUES (?, ?, ?, ?, ?)");
         $insertQuery->bind_param("ssssi", $name, $lastName, $phone, $email, $bookId);
         if ($insertQuery->execute()) {
-            $_SESSION['success'] = 'Pieteikums pievienots!';
-            $_SESSION['successMessage'] = '
+            echo $_SESSION['success'] = 'Pieteikums pievienots!';
+            echo $_SESSION['successMessage'] = '
                 <p class="text-md">Dodieties uz mūsu bibliotēku Ventspils ielā 51. Jums ir 7 dienu laiks, lai grāmatu savāktu!</p>
             ';
         } else {
-            $_SESSION['error'] = 'Kļūda!';
+            echo $_SESSION['error'] = 'Kļūda!';
         }
         $insertQuery->close();
     }
