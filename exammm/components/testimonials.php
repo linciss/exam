0f6bcf540f105f5de19a8971b9ff473ea0c4946e
lincis <linardssils1@gmail.com>
2024-12-05 18:34:05 +0200

    testimonails

---
+++ exammm/components/testimonials.php
@@ -0,0 +1,31 @@
+<section id="testimonials" class="justify-center items-center grid md:grid-cols-3 grid-cols-1   gap-4 mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+    <div class="pt-10 w-full max-w-sm bg-nav dark:bg-darkNav border border-gray-500 rounded-md shadow dark:bg-gray-darkNav mx-auto">
+        <div class="flex flex-col items-center pb-10">
+            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="images/t1.jpg" alt="Ričards image"/>
+            <h5 class="mb-1 text-xl font-medium text-black dark:text-white" id=''>Ričards Bērzs</h5>
+            <span class=" px-2 text-sm text-center text-black dark:text-white">
+                Grāmatas ir mana dzīve. Es nevaru iedomāties savu dzīvi bez tām. Es esmu lasītājs, un esmu lepns par to, ka izmantoju Bibliotēku.
+            </span>
+        </div>
+    </div>
+    <div class="pt-10 w-full max-w-sm bg-nav dark:bg-darkNav border border-gray-500 rounded-md shadow dark:bg-gray-darkNav mx-auto">
+        <div class="flex flex-col items-center pb-10">
+            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="images/t2.jpg" alt="Sintija image"/>
+            <h5 class="mb-1 text-xl font-medium text-black dark:text-white" id=''>Sintija Lapiņa</h5>
+            <span class=" px-2 text-sm text-center text-black dark:text-white">
+                Bibliotēkas mājaslapa ir ļoti ērta un viegli lietojama. Es varu atrast visas grāmatas, ko vēlos izlasīt, un rezervēt tās.
+            </span>
+        </div>
+    </div>
+    <div class="pt-10 w-full max-w-sm bg-nav dark:bg-darkNav border border-gray-500 rounded-md shadow dark:bg-gray-darkNav mx-auto">
+        <div class="flex flex-col items-center pb-10">
+            <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="images/t3.jpg" alt="Harijs image"/>
+            <h5 class="mb-1 text-xl font-medium text-black dark:text-white" id=''>Harijs Lasis</h5>
+            <span class=" px-2 text-sm text-center text-black dark:text-white">
+              Bibliotēkas darbinieki ir ļoti laipni un atsaucīgi. Viņi vienmēr palīdzēs un atbildēs uz visiem jautājumiem. 
+            </span>
+        </div>
+    </div>
+    
+   
+</section>
